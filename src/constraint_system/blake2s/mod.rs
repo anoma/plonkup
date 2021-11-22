1c22cdba410bbd4c59b2b48903dd88e668be30e1
Joshua Fitzgerald <joshuabfitzgerald@gmail.com>
2021-11-22 17:23:05 -0500

    removed WIP extras

--- src/constraint_system/blake2s/mod.rs
+++
@@ -1,5 +0,0 @@
-/// 4-bit chunked blake2s circuit using Plookup  
-pub mod blake2s_4bit;
-
-// /// 8-bit chunked blake2s circuit using Plookup 
-//pub mod blake2s_8bit;
--- src/constraint_system/masp/mod.rs
+++
@@ -1,6 +0,0 @@
-
-/// Circuit used in MASP
-pub mod masp;
-
-/// Constants used in MASP
-pub mod constants;
